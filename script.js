12193c746e5f2ff95610d53981addb232033e5eb
Neo3691102 <nestor8772@gmail.com>
2025-05-26 23:10:26 -0600

    Se agrega la transicion del menu lateral izquierdo y el boton agregar carrito debajo de cada producto

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const closeMenu = document.getElementById("closemenu");
 const menuIzquierdo = document.getElementById("menuIzquierdo");
 
 iconoMenu.addEventListener("click", () => {
- menuIzquierdo.classList.replace("hidden", "visible");
+ menuIzquierdo.classList.add("visible");
   
 });
 
 closeMenu.addEventListener("click", () => {
-  menuIzquierdo.classList.replace("visible", "hidden");
+  menuIzquierdo.classList.remove("visible");
   
 });
